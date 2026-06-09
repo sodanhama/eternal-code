1593f37aa0a5110cb4f1945ead680ce3ba7fb3ef
sodanhama <brainosoham@gmail.com>
2026-06-09 09:46:00 +0530

    input bar works now, have to fix errors

--- packages/cli/src/components/command-menu/commands.tsx
+++ packages/cli/src/components/command-menu/commands.tsx
@@ -7,6 +7,46 @@ export const COMMANDS: Command[] = [
         value: "/new"
     },
     {
+        name: "agents",
+        description: "Switch agents",
+        value: "/agents"    
+    },
+    {
+        name: "models",
+        description: "Switch models",
+        value: "/models"
+    },
+    {
+        name: "sessions",
+        description: "Browse past sessions",
+        value: "/sessions"
+    },
+    {
+        name: "theme",
+        description: "Change color theme",
+        value: "/theme"
+    },
+    {
+        name: "login",
+        description: "Login to your account",
+        value: "/login"
+    },
+    {
+        name: "logout",
+        description: "Logout of your account",
+        value: "/logout"
+    },
+    {
+        name: "upgrade",
+        description: "Buy more credits",
+        value: "/upgrade"
+    },
+    {
+        name: "usage",
+        description: "View your usage and remaining credits",
+        value: "/usage"
+    },
+    {
         name: "exit",
         description: "Exit the application",
         value: "/exit",
--- packages/cli/src/components/input-bar.tsx
+++ packages/cli/src/components/input-bar.tsx
@@ -1,6 +1,11 @@
 import type { KeyBinding } from "@opentui/core";
 import { StatusBar } from "./status-bar";
 import { CommandMenu } from "./command-menu";
+import { useRef, useCallback, useEffect } from "react";
+import type { TextareaRenderable } from "@opentui/core";
+import { useRenderer } from "@opentui/react";
+import type { Command } from "./command-menu/types";
+import { useCommandMenu } from "./command-menu/use-command-menu";
 
 type Props = {
     onSubmit: (text: string) => void;
@@ -14,7 +19,92 @@ export const TEXTAREA_KEY_BINDINGS: KeyBinding[] = [
     {name : "enter", shift: true, action: "newline"},
 ]
 
-export function InputBar({ onSubmit, disabled }: Props) {
+export function InputBar({ onSubmit, disabled = false }: Props) {
+    const textareaRef = useRef<TextareaRenderable>(null);
+    const onSubmitRef = useRef<() => void>(() => {});
+    const renderer = useRenderer();
+
+    const {
+        showCommandMenu,
+        commandQuery,
+        selectedIndex,
+        scrollRef,
+        handleContentChange,
+        resolveCommand,
+        setSelectedIndex,
+    } = useCommandMenu();
+
+
+    const handleCommandExecute = useCallback((
+        index: number
+    ) => {
+        const command = resolveCommand(index);
+        handleCommand(command);
+
+    }, [])
+
+    const handleTextareaContentChange = useCallback(() => {
+        const textarea = textareaRef.current;
+        if (!textarea) return;
+
+        handleContentChange(textarea.plainText);
+
+    }, [])
+
+    const handleSubmit = useCallback(() => {
+        if (disabled) return;
+
+        const textarea = textareaRef.current;
+        if (!textarea) return
+
+        const text = textarea.plainText.trim();
+
+        if (text.length === 0) return;
+
+        onSubmit(text);
+
+        textarea.setText("");
+    }, [disabled, onSubmit]);
+
+    const handleCommand = useCallback((
+        command: Command | undefined
+    )=> {
+        const textarea = textareaRef.current;
+        if (!textarea || !command) return;
+
+        textarea.setText("");
+
+        if (command.action) {
+            command.action({
+                exit: () => renderer.destroy()
+            });
+        } else {
+            textarea.insertText(command.value+' ')
+        }
+        
+    }, [renderer]);
+    
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        if (!textarea) return;
+
+        textarea.onSubmit = () => {
+            onSubmitRef.current();
+        }
+    }, []);
+
+    onSubmitRef.current = () => {
+        if (disabled) return;
+
+        if (showCommandMenu) {
+            const command = resolveCommand(selectedIndex);
+            handleCommand(command);
+            return;
+        }
+
+        handleSubmit();
+    }
+
     return (
         <box width="100%" alignItems="center">
             <box
@@ -29,7 +119,7 @@ export function InputBar({ onSubmit, disabled }: Props) {
                 backgroundColor="#1b263b"
                 width="100%"
                 gap={1}>
-                    {true && (
+                    {showCommandMenu && (
                         <box
                         position="absolute"
                         bottom="100%"
@@ -39,14 +129,20 @@ export function InputBar({ onSubmit, disabled }: Props) {
                         zIndex={10}
                         >
                             <CommandMenu
-                                query=""
+                                query={commandQuery}
+                                selectedIndex={selectedIndex}
+                                scrollRef={scrollRef}
+                                onSelect={setSelectedIndex}
+                                onExecute={handleCommandExecute}
                             />
                         </box>
                     )}
 
                     <textarea
+                    ref={textareaRef}
                     focused={!disabled}
                     keyBindings={TEXTAREA_KEY_BINDINGS}
+                    onContentChange={handleTextareaContentChange}
                     placeholder= {`Ask anything...`}
                     />
                     <StatusBar/>
