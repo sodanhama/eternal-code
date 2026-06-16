5f177135dd16ded6f1021c992d63ac33f23a16ae
sodanhama <brainosoham@gmail.com>
2026-06-16 07:54:54 +0530

    developed shared package

---
+++ packages/shared/src/schemas.ts
@@ -0,0 +1,58 @@
+import { z } from "zod";
+
+export const toolCallArgsSchema = z.record(z.string(), z.json());
+
+export const messagePartSchema = z.discriminatedUnion("type", [
+    z.object({
+        type: z.literal("reasoning"),
+        text: z.string()
+    }),
+    z.object({
+        type: z.literal("tool_call"),
+        id: z.string(),
+        name: z.string(),
+        args: toolCallArgsSchema,
+        results: z.string().optional()
+    }),
+    z.object({
+        type: z.literal("text"),
+        text: z.string()
+    }),   
+])
+
+export const messagePartsSchema = z.array(messagePartSchema);
+
+export type MessagePart = z.infer<typeof messagePartSchema>;
+
+export const chatStreamEventSchema = z.discriminatedUnion("type",[
+    z.object({
+        type: z.literal("text-delta"),
+        text: z.string()
+    }),
+    z.object({
+        type: z.literal("reasons-delta"),
+        text: z.string()
+    }),
+    z.object({
+        type: z.literal("tool-call"),
+        toolCallId : z.string(),
+        toolName : z.string(),
+        args: toolCallArgsSchema
+    }),
+    z.object({
+        type: z.literal("tool-result"),
+        toolCallId : z.string(),
+        result: z.string()
+    }),
+    z.object({
+        type: z.literal("done"),
+        messageId: z.string(),
+        durations: z.number()
+    }),
+    z.object({
+        type: z.literal("error"),
+        message: z.string()
+    }),
+])
+
+export type ChatStreamEvent = z.infer<typeof chatStreamEventSchema>;
