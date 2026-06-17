77df368b94e2863db3279cb4150ac7c9a7b79e2b
sodanhama <brainosoham@gmail.com>
2026-06-17 08:43:41 +0530

    err gone

--- packages/cli/src/lib/api-client.ts
+++ packages/cli/src/lib/api-client.ts
@@ -1,3 +1,6 @@
 import { hc } from "hono/client";
 import type { AppType } from "@eternalcode/server";
 
+export const apiClient = hc<AppType>(
+    process.env.API_URL ?? "http://localhost:3000"
+)
