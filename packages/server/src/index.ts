659cbb268747eab2348773f4663e0c18ae359085
sodanhama <brainosoham@gmail.com>
2026-06-20 16:46:53 +0530

    setup sentry for error monitoring

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,30 @@
 import { Hono } from "hono";
+import { sentry } from "@sentry/hono/bun";
+import * as Sentry from "@sentry/hono/bun"
 import { HTTPException } from "hono/http-exception";
 import sessions from "./routes/sessions";
 
 const app = new Hono();
+
+app.use(
+  sentry(app, {
+    dsn: "https://10894c72dc40807cbc2a49bfc291b18b@o4511597329055744.ingest.de.sentry.io/4511597338099792",
+    tracesSampleRate: 1.0,
+    enableLogs: true,
+    dataCollection: {
+    },
+  }),
+);
+
+app.get("/debug-sentry", () => {
+  // Send a log before throwing the error
+  Sentry.logger.info('User triggered test error', {
+    action: 'test_error_endpoint',
+  });
+  // Send a test metric before throwing the error
+  Sentry.metrics.count('test_counter', 1);
+  throw new Error("My first Sentry error!");
+});
 
 app.onError((error,c)=>{
     if (error instanceof HTTPException){
--- packages/server/src/routes/sessions.ts
+++ packages/server/src/routes/sessions.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
-import { db } from "@eternalcode/database";
+import { db } from "@eternalcode/database/client";
 import { Role, Mode, MessageStatus } from "@eternalcode/database/enums";
 import { findSupportedChatModel } from "@eternalcode/shared"
 
